ff97e1424e80ca6b5c403453b486dbc1583cfe40
何军 <4240249@qq.com>
2015-08-10 11:18:51 +0800

    微信分享js修改

--- js/share.js
+++ js/share.js
@@ -1,39 +1,40 @@
 /**
  * Created by hejun on 15/4/15.
  *  纯前端配置-微信分享
- *  1.加载js后系统会自动加载微信分享相关js
+ *  1.加载js后 请先执行init方法
  *  2.当前只能在socialpark.com.cn 域名下使用 其他域名使用无效(受公众账号设置限制)
  */
 !(function wxshare() {
     window.tp = window.tp || {};
-    window.tp.wx={};
-    function loadScript(url, callback) {
-        var script = document.createElement("script")
-        script.type = "text/javascript";
-        if (script.readyState) { //IE 
-            script.onreadystatechange = function() {
-                if (script.readyState == "loaded" ||
-                    script.readyState == "complete") {
-                    script.onreadystatechange = null;
-                    callback();
-                }
-            };
-        } else { //Others: Firefox, Safari, Chrome, and Opera 
-            script.onload = function() {
-                callback();
-            };
-        }
-        script.src = url;
-        document.body.appendChild(script);
+    window.tp.wx = {};
+    var shareData = {
+        title: '',
+        desc: '',
+        link: '',
+        imgUrl: '',
+        success: function() {},
+        cancel: function() {}
+    };
+    var isDebug=!1;
+    /**
+     * 分享初始化
+     * @param  {[type]} defaultshareData  默认分享文案
+     * @param  {[type]} debug             调试是否打开 默认false
+     */
+    window.tp.wx.init = function(defaultshareData,debug) {
+        defaultshareData = defaultshareData || {};
+        shareData = extend(shareData, defaultshareData);
+        debug=debug||isDebug;
+        isDebug=!!Debug;
+        loadScript("http://res.wx.qq.com/open/js/jweixin-1.0.0.js", function() {
+            var url = "http://www.socialpark.com.cn/wechat/getshare.php?t=" + new Date().getTime() + "&callback=tp.wx.config&url=" + encodeURIComponent(location.href.replace(location.hash, ""));
+            loadScript(url);
+        });
     }
-    loadScript("http://res.wx.qq.com/open/js/jweixin-1.0.0.js",function(){
-        var url = "http://www.socialpark.com.cn/wechat/getshare.php?t=" + new Date().getTime() + "&callback=tp.wx.config&url=" + encodeURIComponent(location.href.replace(location.hash, ""));
-        loadScript(url);
-    });
-    
+
     window.tp.wx.config = function(d) {
         wx.config({
-            //debug    : true,
+            debug    : isDebug,
             appId: d.appid,
             timestamp: d.timestamp,
             nonceStr: d.noncestr,
@@ -64,31 +65,9 @@
         });
     }
 
+
     window.tp.wx.setshare = function(d) {
         d = d || {};
-        var shareData = {
-            title: '报告组织！我的素质击败了杭城0%的人，回去看书！',
-            desc: '原来这就是成为全国素质最高城市的秘诀！这个世界不只看颜值，我对世界不失望！',
-            link: 'http://yxh.realty.socialpark.com.cn/visa/?' + Math.random(),
-            imgUrl: 'http://yxh.realty.socialpark.com.cn/visa/favicon.ico',
-            success: function() {
-
-            }
-        };
-
-        function extend(target, source) {
-            target = target || {};
-            var result = {};
-            for (var p in target) {
-                if (source.hasOwnProperty(p)) {
-                    result[p] = source[p];
-                } else {
-                    result[p] = target[p];
-                }
-            }
-            return result;
-        }
-
         shareData = extend(shareData, d);
         wx.hideMenuItems({
             menuList: ['menuItem:share:qq', 'menuItem:share:weiboApp', 'menuItem:share:facebook']
@@ -104,8 +83,14 @@
             success: function() {
                 shareData.success && shareData.success();
                 try {
-                    _hmt.push(['_trackEvent', "分享成功", '分享成功']);
+                    _hmt.push(['_trackEvent', "分享成功", '分享到朋友圈']);
                 } catch (e) {}
+            },
+            cancel: function() {
+                shareData.cancel && shareData.cancel();
+                try {
+                    _hmt.push(['_trackEvent', "取消分享", '取消分享']);
+                }
             }
         });
         // 发送给指定微信好友
@@ -117,10 +102,68 @@
             success: function() {
                 shareData.success && shareData.success();
                 try {
-                    _hmt.push(['_trackEvent', "分享成功", '分享成功']);
+                    _hmt.push(['_trackEvent', "分享成功", '分享给好友']);
+                } catch (e) {}
+            },
+            cancel: function() {
+                shareData.cancel && shareData.cancel();
+                try {
+                    _hmt.push(['_trackEvent', "取消分享", '取消分享']);
                 } catch (e) {}
             }
         });
+        wx.onMenuShareQQ({
+            title: shareData.title, // 分享标题
+            desc: shareData.desc, // 分享描述
+            link: shareData.link, // 分享链接
+            imgUrl: shareData.imgUrl, // 分享图标
+            success: function() {
+                shareData.success && shareData.success();
+                try {
+                    _hmt.push(['_trackEvent', "分享成功", '分享成功']);
+                }
+                c
+            },
+            cancel: function() {
+                shareData.cancel && shareData.cancel();
+                try {
+                    _hmt.push(['_trackEvent', "取消分享", '取消分享']);
+                }
+            }
+        });
+    }
+
+    function loadScript(url, callback) {
+        var script = document.createElement("script")
+        script.type = "text/javascript";
+        if (script.readyState) { //IE 
+            script.onreadystatechange = function() {
+                if (script.readyState == "loaded" ||
+                    script.readyState == "complete") {
+                    script.onreadystatechange = null;
+                    callback();
+                }
+            };
+        } else { //Others: Firefox, Safari, Chrome, and Opera 
+            script.onload = function() {
+                callback();
+            };
+        }
+        script.src = url;
+        document.body.appendChild(script);
+    }
+
+    function extend(target, source) {
+        target = target || {};
+        var result = {};
+        for (var p in target) {
+            if (source.hasOwnProperty(p)) {
+                result[p] = source[p];
+            } else {
+                result[p] = target[p];
+            }
+        }
+        return result;
     }
 
 })();
